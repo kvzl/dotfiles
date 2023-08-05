11c360198a81d2b031a41e33eb0ea71fc7843baa
kvzl <kvzly0u@gmail.com>
2023-08-06 02:23:58 +0800

    Add Sketchybar

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -44,7 +44,7 @@
 (setq evil-shift-width 2)
 
 ;; Fonts
-(setq-default line-spacing 2)
+;; (setq-default line-spacing 0)
 ;; (set-face-attribute 'default nil :font "Jetbrains Mono" :height 120)  ;; should be called after initializing ligature.el
 
 (let ((frame-transparency '(90 . 90)))
