8aa42811ba69228e84064d9ddc4493e5b9953e27
kvzl <kvzly0u@gmail.com>
2025-10-01 00:12:40 +0800

    Update nvim theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "catppuccin/nvim",
-  lazy = true,
-  name = "catppuccin",
-  opts = {
-    flavor = "mocha",
-    transparent_background = true,
-  },
-}
--- .config/nvim/lua/plugins/nordic.lua
+++ .config/nvim/lua/plugins/nordic.lua
@@ -4,18 +4,18 @@ return {
   priority = 1000,
   config = function()
     require("nordic").load({
+      on_highlight = function(highlights, palette)
+        highlights.CursorLine.bg = palette.black0
+      end,
       transparent = {
         bg = true,
         float = true,
       },
+      bold_keywords = true,
       bright_border = true,
       reduced_blue = true,
-      swap_backgrounds = true,
       cursorline = {
-        theme = "dark",
-      },
-      telescope = {
-        style = "classic",
+        theme = "light",
       },
     })
   end,
